b4b5916bcda48dc813f8adfdcc6df9d839cb694b
Keidi-Teixeira <99774507+Keidi-Teixeira@users.noreply.github.com>
2022-03-02 18:12:05 -0300

    Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+public class Main {
+public static void main(String[] args) {
+
+String nome = "Keidi";
+
+byte idade = 15;
+char gender = 'F';
+double salario = 200.0;
+
+System.out.println("Procurando estágio");
+
+System.out.printf("%s, estudante do ensino médio técnico em Desenvolvimento de Sistemas com %d anos  procura estágio, expectativa de ganhar R$ %.2f%n reais%n", nome, idade, salario);
+
+
+}
+}
